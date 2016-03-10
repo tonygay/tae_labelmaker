0cf5b41b2073f71c17b083db3b73d77e399380ec
Jay Lankford <jlnkfrd@gmail.com>
2016-03-09 22:44:07 -0600

    Allow clearing of To field when creating labels.

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -6,4 +6,7 @@ $( document ).ready(function() {
     console.log($.fn.tooltip.Constructor.VERSION);
 });
 
-$('select.select2').select2();
+$('select.select2').select2({
+	placeholder: 'Start typing to select an institution...',
+	allowClear: true
+});
